24b4b2673c6b88b0023a62079106a1e90bc6ad21
seed-yoo <lukdown10@naver.com>
2023-12-26 16:28:50 +0900

    swich_case문 연습

--- src/com/javaex/ex03/Ex11.java
+++ src/com/javaex/ex03/Ex11.java
@@ -13,14 +13,46 @@ public class Ex11 {
 		
 		switch (m) {
 		case 1:
-			System.out.println("31일 입니다");
+			System.out.println("31일 입니다.");
+			break;
+		case 2:
+			System.out.println("28일 입니다.");
+			break;
+		case 3:
+			System.out.println("31일 입니다.");
+			break;
+		case 4:
+			System.out.println("30일 입니다.");
+			break;
+		case 5:
+			System.out.println("31일 입니다.");
+			break;
+		case 6:
+			System.out.println("30일 입니다.");
+			break;
+		case 7:
+			System.out.println("31일 입니다.");
+			break;
+		case 8:
+			System.out.println("31일 입니다.");
+			break;
+		case 9:
+			System.out.println("30일 입니다.");
+			break;
+		case 10:
+			System.out.println("31일 입니다.");
+			break;
+		case 11:
+			System.out.println("30일 입니다.");
+			break;
+		case 12:
+			System.out.println("31일 입니다.");
 			break;
 
 		default:
+			System.out.println("1~12의 숫자가 아닙니다.");
 			break;
 		}
-		
-		
 		
 		
 		
